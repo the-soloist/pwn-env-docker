c44b838c23c44c776e1fcaa5b593a8ed1f200952
Th3S <46804083+the-soloist@users.noreply.github.com>
2023-02-05 17:12:54 +0800

    feat: add install-proxy-tools.sh

---
+++ scripts/install-proxy-tools.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage:  $0 <container-id> <host> <port>"
+    echo "  e.g.: $0 <id> 127.0.0.1 10808"
+    exit 1
+fi
+
+WORK_HOME="$(dirname $(realpath $0))"
+CONTAINER_ID="$1"
+HOST="$2"
+PORT="$2"
+
+set -x
+
+docker exec -it $1 bash -c "apt-get update"
+docker exec -it $1 bash -c "apt-get install proxychains4"
+docker exec -it $1 bash -c "sed -i '/^socks4/csocks5 $HOST $PORT' /etc/proxychains4.conf"
