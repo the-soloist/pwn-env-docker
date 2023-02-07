a095505e971b53a5cc7f758fdfd26219476ec8a0
Th3S <46804083+the-soloist@users.noreply.github.com>
2023-02-07 17:31:37 +0800

    fix: modity shell parameters

---
+++ scripts/shell.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage:  $0 <container-id> <shell-type>"
+    echo "  e.g.: $0 <id> bash"
+
+    echo -e "\n>>> docker ps"
+    docker ps
+
+    exit 1
+fi
+
+CONTAINER_ID="$1"
+SHELL_TYPE="$2"
+
+docker exec -it $CONTAINER_ID $SHELL_TYPE
