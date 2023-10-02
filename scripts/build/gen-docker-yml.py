17550025ed4b00075c721a7a0261c4e86cd9573d
Th3S <46804083+the-soloist@users.noreply.github.com>
2023-10-02 16:33:36 +0800

    chore: categorize ubuntu version

--- scripts/build/gen-docker-yml.py
+++ scripts/build/gen-docker-yml.py
@@ -6,10 +6,12 @@ import yaml
 from pathlib import Path
 
 
-workhome = Path(__file__).parent.parent
+workhome = Path(__file__).parent.parent.parent
 ubuntu_file = workhome / "build" / "ubuntu"
-ubuntu_version = [path.name for path in ubuntu_file.glob('*') if path.is_dir()]
-
+ubuntu_version = {
+    "LTS": [path.name for path in ubuntu_file.glob("lts/*") if path.is_dir()],
+    "non-LTS": [path.name for path in ubuntu_file.glob("non-lts/*") if path.is_dir()]
+}
 
 TEMPLATE_YAML = """
   ubuntu-{version}:
@@ -34,7 +36,7 @@ TEMPLATE_YAML_DEV = """
     container_name: pwn-env_ubuntu-{version}
     build:
       context: ./
-      dockerfile: ./build/ubuntu/{version}/Dockerfile
+      dockerfile: ./build/ubuntu/{type}/{version}/Dockerfile
     volumes:
       - ./challenge:/challenge
       - ./config:/root/.config
@@ -52,12 +54,17 @@ docker_compose = """version: '3'\n\nservices:"""
 docker_compose_dev = """version: '3'\n\nservices:"""
 
 
-for version in sorted(ubuntu_version):
+for version in sorted(ubuntu_version["LTS"] + ubuntu_version["non-LTS"]):
     print(version)
     _ver = version.replace(".", "")
 
+    if version in ubuntu_version["LTS"]:
+        ubuntu_type = "lts"
+    else:
+        ubuntu_type = "non-lts"
+
     docker_compose += TEMPLATE_YAML.format(version=version, ver3=_ver[:3], ver4=_ver[:4])
-    docker_compose_dev += TEMPLATE_YAML_DEV.format(version=version, ver3=_ver[:3], ver4=_ver[:4])
+    docker_compose_dev += TEMPLATE_YAML_DEV.format(version=version, type=ubuntu_type, ver3=_ver[:3], ver4=_ver[:4])
 
 
 open("docker-compose.yml", "w").write(docker_compose)
