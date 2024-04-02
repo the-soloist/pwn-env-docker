4b730e13ac2cc995680a93f3b9791bfbe9c76d85
Th3S <46804083+the-soloist@users.noreply.github.com>
2024-04-02 19:27:23 +0800

    feat: update build scripts

--- scripts/build-lts-images.sh
+++ scripts/build-lts-images.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
-set -x
+# set -x
+
+function print_help() {
+    echo "Usage:"
+    echo "  $0"
+    echo "  '-f': remove images"
+    exit 0
+}
+
+function remove_images() {
+    docker rmi th3s/pwn-env:ubuntu-16.04
+    docker rmi th3s/pwn-env:ubuntu-18.04
+    docker rmi th3s/pwn-env:ubuntu-20.04
+    docker rmi th3s/pwn-env:ubuntu-22.04
+}
 
 docker compose down
-docker rmi th3s/pwn-env:ubuntu-16.04
-docker rmi th3s/pwn-env:ubuntu-18.04
-docker rmi th3s/pwn-env:ubuntu-20.04
-docker rmi th3s/pwn-env:ubuntu-22.04
+
+while getopts "hf" OPT; do
+    case $OPT in
+    f) remove_images ;;
+    h) print_help ;;
+    esac
+done
+
 docker compose pull ubuntu-16.04
 docker compose pull ubuntu-18.04
 docker compose pull ubuntu-20.04
