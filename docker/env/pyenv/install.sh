e537f08081b45cecb96e0a254f2f5219785773a4
Th3S <46804083+the-soloist@users.noreply.github.com>
2024-10-21 21:31:03 +0800

    feat(env): update script

--- docker/env/pyenv/install.sh
+++ docker/env/pyenv/install.sh
@@ -7,4 +7,7 @@ export PYENV_ROOT="/opt/pyenv"
 curl https://pyenv.run | bash
 
 apt update
-apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev
+apt install -y make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncurses5-dev libncursesw5-dev xz-utils tk-dev \
+    libffi-dev liblzma-dev libssl-dev
