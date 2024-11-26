fc7ecb1e84cf63f088816752dddd487c77f06a86
Th3S <46804083+the-soloist@users.noreply.github.com>
2024-11-26 19:42:22 +0800

    feat: update scripts

--- docker/env/pyenv/python-3.8.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-set -x
-
-export PYENV_ROOT="/opt/pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(command pyenv init -)"
-
-pyenv install 3.8.19
