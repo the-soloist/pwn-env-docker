34943531d82dc8dc79c95de31ea3f8df3525daa7
Th3S <46804083+the-soloist@users.noreply.github.com>
2024-11-30 14:42:04 +0800

    feat: set pyenv version

---
+++ docker/env/pyenv/global.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+set -x
+
+function print_help() {
+    echo "Usage:  $0 -p <python-version>"
+    echo "  e.g.: $0 -p 3.8.16"
+    exit 1
+}
+
+while getopts "p:h" OPT; do
+    case $OPT in
+    p) PY_VERSION="$OPTARG" ;;
+    h) print_help ;;
+    ?) print_help ;;
+    esac
+done
+
+export PYENV_ROOT="/opt/pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(command pyenv init -)"
+
+pyenv global $PY_VERSION
