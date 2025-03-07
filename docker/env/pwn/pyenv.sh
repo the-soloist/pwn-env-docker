90b12fd32867377de6bc447bfdf292b8599a45d2
Th3S <46804083+the-soloist@users.noreply.github.com>
2025-03-07 20:53:25 +0800

    refactor: update pyenv installation and tool setup scripts
    - Replaced `/opt/env/pyenv/base.sh` with new `/opt/env/pyenv/deps.sh` script
    - Modified Dockerfiles for Ubuntu LTS and non-LTS versions to use new deps script
    - Updated `docker/env/pwn/pyenv.sh` to use a separate python.sh script for tool installation
    - Simplified pyenv and Python tool installation process across different Ubuntu versions

--- docker/env/pwn/pyenv.sh
+++ docker/env/pwn/pyenv.sh
@@ -27,8 +27,5 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-pip3 install pip --upgrade
-pip3 install prettytable colorama loguru tqdm
-pip3 install ipdb websocket-client psutil requests redis
-pip3 install gmpy2 pycryptodome
-pip3 install z3-solver angr pwntools
+cd $(dirname $0)
+bash ./python.sh
