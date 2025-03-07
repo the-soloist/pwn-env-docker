38c295a980eb95f90e851171c8196d8b8090109c
Th3S <46804083+the-soloist@users.noreply.github.com>
2025-03-07 21:01:39 +0800

    refactor: consolidate Python tool installation in pyenv.sh
    - Merged python.sh contents directly into pyenv.sh
    - Removed redundant python.sh script
    - Simplified Python tool installation process
    - Kept existing pip package installations intact

--- docker/env/pwn/pyenv.sh
+++ docker/env/pwn/pyenv.sh
@@ -27,5 +27,8 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-cd $(dirname $0)
-bash ./python.sh
+pip3 install pip --upgrade
+pip3 install prettytable colorama loguru tqdm
+pip3 install ipdb websocket-client psutil requests redis
+pip3 install gmpy2 pycryptodome
+pip3 install z3-solver angr pwntools
--- docker/env/pwn/python.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-set -x
-
-pip3 install pip --upgrade
-pip3 install prettytable colorama loguru tqdm
-pip3 install ipdb websocket-client psutil requests redis
-pip3 install gmpy2 pycryptodome
-pip3 install z3-solver angr pwntools
